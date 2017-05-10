629d79683d59ecab40c9d0176e7a763fe98e56e6
David Jansen <david.awam.jansen@gmail.com>
2017-05-10 16:55:14 +0000

    R code for nmds plot fixed

--- analysis_driver.bash
+++ analysis_driver.bash
@@ -43,4 +43,4 @@ code/mothur/mothur code/get_shared_otus.batch
 code/mothur/mothur code/get_nmds_data.batch
 
 # Generate ndms plot as png file
-R -e "source('code/plot_ndms.R'); plot_ndms('data/mothur/stability.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.pick.opti_mcc.thetayc.0.03.lt.ave.nmds.axes')"
+R -e "source('code/plot_nmds.R'); plot_nmds('data/mothur/stability.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.pick.opti_mcc.thetayc.0.03.lt.ave.nmds.axes')"
